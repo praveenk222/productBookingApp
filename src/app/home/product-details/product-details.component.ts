0565b39cfd70a346028ce888a6498f52ce75b065
satheesh-kumar1285 <satheeshkumar7366@gmail.com>
2024-03-04 21:25:33 -0600

    imageslide

--- src/app/home/product-details/product-details.component.ts
+++ src/app/home/product-details/product-details.component.ts
@@ -1,4 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+
+import { Component, HostListener, Input, OnInit, inject } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable, Subscription, interval, of, timer } from 'rxjs';
+import { catchError, map, shareReplay } from 'rxjs/operators';
+import { ViewChild, ElementRef } from '@angular/core';
+import { InfiniteScrollCustomEvent, IonContent, PopoverController } from '@ionic/angular';
+import { Injectable, NgZone } from '@angular/core';
+import { filter, scan } from 'rxjs/operators';
+import { Swiper } from 'swiper';
+import { withEnabledBlockingInitialNavigation } from '@angular/router';
 
 @Component({
   selector: 'app-product-details',
@@ -6,9 +16,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-details.component.scss'],
 })
 export class ProductDetailsComponent  implements OnInit {
-
+  quantity: number = 0;
   constructor() { }
 
   ngOnInit() {}
+  
+  incrementQuantity() {
+    this.quantity++;
+  }
 
+  decrementQuantity() {
+    if (this.quantity > 0) {
+      this.quantity--;
+    }
+  }
+
+  images = [
+    'https://manybooks.net/sites/default/files/styles/220x330sc/public/webform/ebook_feature_application/7996/galaxy_bridge_01_forward_command_final.jpg?itok=TQsywqWl' ,
+    'https://manybooks.net/sites/default/files/styles/220x330sc/public/webform/ebook_feature_application/8306/wm_ebook.jpg?itok=UvWKNNlN',
+    'https://manybooks.net/sites/default/files/styles/220x330sc/public/webform/ebook_feature_application/9021/the_baby_farm_murders_ebook.jpeg?itok=s5O8SbHR',
+    'https://manybooks.net/sites/default/files/styles/220x330sc/public/webform/ebook_feature_application/8781/the-origin-of-dreams-coverfront-%5B12-10-23%5D.jpg?itok=z553KW6L',
+    'https://manybooks.net/sites/default/files/styles/220x330sc/public/webform/ebook_feature_application/8821/91awfmnc1ul._sl1500_.jpg?itok=brVLW0fG',
+    'https://manybooks.net/sites/default/files/styles/220x330sc/public/2023-12/91qRbebr-zL._SL1500_.jpg?itok=eqzqoiy2',
+    'https://manybooks.net/sites/default/files/styles/220x330sc/public/2023-10/8157SgW4qjL._SL1500_.jpg?itok=rCRvVJaq',
+    
+  ];
+ 
+  
+ 
+  swiperSlideChanged(e: any) {
+   console.log('changed: ', e);
+ }
+ 
+
+ onIonInfinite(ev:any) {
+   setTimeout(() => {
+     (ev as InfiniteScrollCustomEvent).target.complete();
+   }, 500);
+ }
+ 
 }
