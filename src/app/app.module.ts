f8b17a2dc08b6d241b41872dce85ed90296c2dbf
nimmalapalli <nimmalapallinarendra@gmail.com>
2024-03-11 22:21:33 +0530

    code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,15 +18,12 @@ import { FolderPage } from './folder/folder.page';
     BrowserModule, 
     IonicModule.forRoot(),  
     AppRoutingModule,
-    SharedModule,
+ 
     IonicModule,
-    HttpClientModule,
-    IonicStorageModule.forRoot({ name: "mylocaldb" }),
-    BrowserAnimationsModule,
+  
     ],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },MatDatepickerModule,
-    { provide: MAT_SNACK_BAR_DEFAULT_OPTIONS, useValue: { duration: 3000 ,    horizontalPosition: 'end',
-    verticalPosition: 'bottom',} },
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+ 
   ],
   bootstrap: [AppComponent],
   schemas:[CUSTOM_ELEMENTS_SCHEMA]
