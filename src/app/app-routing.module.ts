19af29010b3e2cea1c498192c49ccdff7c31c6e4
Reddy0727 <brahmareddy8242@gmail.com>
2024-03-14 12:30:21 +0530

    incrementModify in mybag

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -19,7 +19,7 @@ const routes: Routes = [
     RouterModule.forRoot([
       {
        path:'',
-       component:MainhomePage
+       component:MyBagPage
       },
       {
         path: 'folder',
--- src/app/my-bag/my-bag.page.ts
+++ src/app/my-bag/my-bag.page.ts
@@ -7,29 +7,30 @@ import { CartService } from '../services/cart.service';
   styleUrls: ['./my-bag.page.scss'],
 })
 export class MyBagPage implements OnInit {
-  itemCount:number=0;
+
   constructor(private cart:CartService) { }
 
   ngOnInit() {
-    alert()
+   
   }
-  baglist=[
-    {bagImg:'../../assets/jeans.webp',type:'Men Jeans',price:'1999',color:'Grey',size:'L'},
-    {bagImg:'../../assets/wishWatch.webp',type:'Luminox Mens Sea',price:'2999',color:'Blue',size:'M'},
-  ]
   billAmount=[
     {total:'1500',couponCode:'150',gst:'50',dCharge:'50'}
   ]
-  itemIncre() {
-    this.itemCount++
+  baglist=[
+    {id:1,bagImg:'../../assets/jeans.webp',type:'Men Jeans',price:'1999',color:'Grey',size:'L',itemCount:0},
+    {id:2,bagImg:'../../assets/wishWatch.webp',type:'Luminox Mens Sea',price:'2999',color:'Blue',size:'M',itemCount:0},
+  ]
+  itemIncre(id: any) {
+    return this.baglist.filter(bag => bag.id === id ? bag.itemCount++ : bag.itemCount)
   }
-  itemDecre() {
-    return this.itemCount ? this.itemCount-- : this.itemCount
+  itemDecre(id:number) {
+    return this.baglist.filter(bag => bag.id === id ? bag.itemCount-- : bag.itemCount)
   }
   parseToInt(value: string): number {
     return parseInt(value, 10); 
   }
   getItems(){
    this.baglist=this.cart.getItems()
+   console.log(this.baglist)
   }
 }
